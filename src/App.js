66600955f184e23f2d547bd3899ce53ee014719b
Charles Kang <kscscy@gmail.com>
2020-08-12 19:17:19 +0900

    styling-react commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
-import SassComponent from './SassComponent'
+import StyledComponent from './StyledComponent';
 
+
+// import styles from './CSSModule.module.scss'
+// import classNames from 'classnames/bind'
+// import CSSModule from './CSSModule'
+// import SassComponent from './SassComponent'
 // import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
@@ -48,11 +53,24 @@ import SassComponent from './SassComponent'
 //   }
 // }
 
-export class App extends Component {
+// const cx = classNames.bind(styles); // 미리 styles 에서 클래스를 받아 오도록 설정
+
+// export class App extends Component {
+//   render() {
+//     return (
+//       // <div className={`${styles.wrapper} ${styles.inverted}`}>
+//       <div className={cx('wrapper', 'inverted')}>
+//         안녕하세요, 저는 <span className="something">CSS Module!!</span>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />        
       </div>
     );
   }
